4a783f4834289d06c7a4f7c8119c6959cac857c9
iwtcode <iwtcode@gmail.com>
2025-01-01 22:41:21 +0300

    init

---
+++ src/test/java/org/example/course_work/CourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.course_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
